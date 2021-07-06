80163d04cf394c9cec5815fbfe65d70d78806c07
Your Name <you@example.com>
2021-07-06 16:23:35 -0400

    first push

---
+++ src/test/java/com/dhaka/OrangeHrmOpen.java
@@ -0,0 +1,29 @@
+package com.dhaka;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OrangeHrmOpen {
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) { 
+		   // System Property for Chrome Driver   
+        System.setProperty("webdriver.chrome.driver", "C:\\Driver\\chromedriver.exe"); 
+          
+             // Instantiate a ChromeDriver class.     
+        WebDriver driver=new ChromeDriver();  
+          
+           // Launch Website  
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/index.php/auth/login");  
+            
+     
+     
+
+	}
+
+}
